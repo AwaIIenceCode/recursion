b1baf7e11096185fed78a0f88e69850d2b0a48b9
Oleh Skrypnyk-Zakrevskiy <awallencecode@gmail.com>
2024-12-28 20:23:49 +0200

    ex 3 is done

--- main.cpp
+++ main.cpp
@@ -1,21 +1,69 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <string>
 
 using namespace std;
 
-int gcd(int a, int b)
+void generateNumber(string& secret)
 {
-    if (b == 0)
-        return a;
+    srand(time(NULL));
+    secret = "";
+    while (secret.length() < 4)
+    {
+        char digit = '0' + rand() % 10;
+        if (secret.find(digit) == string::npos)
+        {
+            secret += digit;
+        }
+    }
+}
 
-    return gcd(b, a % b);
+void checkGuess(const string& secret, const string& guess, int& bulls, int& cows)
+{
+    bulls = 0;
+    cows = 0;
+    for (int i = 0; i < 4; i++)
+    {
+        if (guess[i] == secret[i])
+        {
+            bulls++;
+        } else if (secret.find(guess[i]) != string::npos)
+        {
+            cows++;
+        }
+    }
+}
+
+void playGame(const string& secret, int attempts = 0)
+{
+    string guess;
+    cout << "Enter your guess: ";
+    cin >> guess;
+
+    int bulls, cows;
+    checkGuess(secret, guess, bulls, cows);
+
+    if (bulls == 4)
+    {
+        cout << "Congratulations! You guessed the number in " << attempts + 1 << " attempts.\n";
+        return;
+    }
+
+    else
+    {
+        cout << "Bulls: " << bulls << ", Cows: " << cows << endl;
+        playGame(secret, attempts + 1);
+    }
 }
 
 int main()
 {
-    int a, b;
-    cout << "Enter two integers: ";
-    cin >> a >> b;
+    string secret;
+    generateNumber(secret);
+    cout << "I have generated a 4-digit number. Try to guess it!\n";
 
-    cout << "Greatest Common Divisor: " << gcd(a, b) << endl;
+    playGame(secret);
+
     return 0;
 }
