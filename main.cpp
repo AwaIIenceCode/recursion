d20ad7f4ba93931de3a24fbf13cfaf38f945cb8c
Oleh Skrypnyk-Zakrevskiy <awallencecode@gmail.com>
2024-12-28 20:19:11 +0200

    ex 2 is done

--- main.cpp
+++ main.cpp
@@ -1,79 +1,21 @@
 #include <iostream>
-#include <algorithm>
-#include <ctime>
 
 using namespace std;
 
-template <typename T>
-void initializeMatrix(T** matrix, int size)
+int gcd(int a, int b)
 {
-    srand(time(NULL));
-    for (int i = 0; i < size; i++)
-    {
-        for (int j = 0; j < size; j++)
-        {
-            matrix[i][j] = static_cast<T>(rand() % 10);
-        }
-    }
-}
+    if (b == 0)
+        return a;
 
-template <typename T>
-void printMatrix(T** matrix, int size)
-{
-    for (int i = 0; i < size; i++)
-    {
-        for (int j = 0; j < size; j++)
-        {
-            cout << matrix[i][j] << " ";
-        }
-        cout << endl;
-    }
-}
-
-template <typename T>
-void findMinMaxDiagonal(T** matrix, int size, T& min, T& max)
-{
-    min = matrix[0][0];
-    max = matrix[0][0];
-    for (int i = 0; i < size; i++)
-    {
-        if (matrix[i][i] < min) min = matrix[i][i];
-        if (matrix[i][i] > max) max = matrix[i][i];
-    }
-}
-
-template <typename T>
-void sortRows(T** matrix, int size)
-{
-    for (int i = 0; i < size; i++)
-    {
-        sort(matrix[i], matrix[i] + size);
-    }
+    return gcd(b, a % b);
 }
 
 int main()
 {
-    int size;
-    cout << "Enter the size of the matrix: ";
-    cin >> size;
+    int a, b;
+    cout << "Enter two integers: ";
+    cin >> a >> b;
 
-    int** intMatrix = new int*[size];
-    for (int i = 0; i < size; i++) intMatrix[i] = new int[size];
-
-    initializeMatrix(intMatrix, size);
-    cout << "Integer Matrix:\n";
-    printMatrix(intMatrix, size);
-
-    int intMin, intMax;
-    findMinMaxDiagonal(intMatrix, size, intMin, intMax);
-    cout << "Integer Matrix Diagonal Min: " << intMin << ", Max: " << intMax << endl;
-
-    sortRows(intMatrix, size);
-    cout << "Sorted Integer Matrix:\n";
-    printMatrix(intMatrix, size);
-
-    for (int i = 0; i < size; i++) delete[] intMatrix[i];
-    delete[] intMatrix;
-
+    cout << "Greatest Common Divisor: " << gcd(a, b) << endl;
     return 0;
 }
